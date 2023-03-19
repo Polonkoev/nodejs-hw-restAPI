a17d554d83c296d5292856b3c6e402f2c33166eb
Polonkoev <103013345+Polonkoev@users.noreply.github.com>
2023-03-19 22:04:29 +0600

    Finish

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 require("./middlewares/config-passport");
 
 const contactsRouter = require("./api/contacts");
-const usersRouter = require("./api/users.js");
+const usersRouter = require("./api/users/users");
 
 const app = express();
 
@@ -16,7 +16,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+
 app.use("/api/users", usersRouter);
+app.use("/avatars", express.static("public/avatars"));
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
